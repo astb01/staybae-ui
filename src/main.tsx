7c84a3c6c474875d1e4c40e014f34a2a6b7bb2e5
Assad Ahmad <astb01@gmail.com>
2024-02-06 19:07:02 +0000

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
+import '../styles/global.scss';
+
+import { BrowserRouter } from 'react-router-dom';
+import { FavouriteContextProvider } from './context/FavouritesContext';
+
+async function enableMocking() {
+  const { worker } = await import('./__mocks__/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <FavouriteContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </FavouriteContextProvider>
+    </React.StrictMode>,
+  );
+});
